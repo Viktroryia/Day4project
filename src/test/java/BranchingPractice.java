d785f0015c754bfd4cc5aa5513b14caaa6ca0199
Viktroryia <madvika89@gmail.com>
2021-05-25 18:23:45 -0500

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,11 @@
 public class BranchingPractice {
 
     public static void main(String[] args) {
+        //created new project
+        // enabled  origin control
+        //added .gitignore with .idea folder .iml file
+        // made initial commit by keeping track of a;ll file
+
 
     }
 }
